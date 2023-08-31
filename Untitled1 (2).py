36c2cc3e473fdecb72c696f0d118b8edec00664c
yogeesh0512 <142562038+yogeesh0512@users.noreply.github.com>
2023-08-31 10:56:42 +0530

    Add files via upload

---
+++ Untitled1 (2).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+df=pd.read_csv('C:\\Users\\GPT BANTWAL\\Downloads\\weather12.csv')
+df
+
+
+# In[6]:
+
+
+import pandas as pd
+df=pd.read_csv('C:\\Users\\GPT BANTWAL\\Downloads\\list_of_cities_and_towns_in_india-834j1.csv')
+df
+
+
+# In[ ]:
+
+
+
+
