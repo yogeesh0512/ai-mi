e0aeee4d56adb8face575a225b4e73f006383c45
yogeesh0512 <142562038+yogeesh0512@users.noreply.github.com>
2023-08-31 10:00:12 +0530

    weatheer car.auto

---
+++ Untitled (1).py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+df=pd.read_csv('C:\\Users\\GPT BANTWAL\\Downloads\\mtcars .csv')
+df
+
+
+# In[13]:
+
+
+import pandas as pd
+df=pd.read_csv('C:\\Users\\GPT BANTWAL\\Downloads\\rainfall1.csv')
+df
+
+
+# In[14]:
+
+
+import pandas as pd
+df=pd.read_csv('C:\\Users\\GPT BANTWAL\\Downloads\\auto_mpg11.csv')
+df
+
+
+# In[ ]:
+
+
+
+
